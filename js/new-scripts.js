eb721dfb50bca61a714e56fa71973edad2fa71c4
javohir1993 <javohirchegabayev@gmail.com>
2021-01-12 11:50:51 +0500

    Create function for show display and code impove

--- js/new-scripts.js
+++ js/new-scripts.js
@@ -20,13 +20,47 @@ var createElement = function (element, elementClass, text) {
   return newElement;
 };
 
+
+var displayResults = function (forRun = pokemons) {
+
+  elResultsList.innerHTML = '';
+  var elForBasicTemplateFragment = document.createDocumentFragment();
+
+  forRun.forEach(function (pokemon) {
+    var elBasicTemplateClone = elBasicTemplate.cloneNode(true);
+
+    elResultsList.innerHTML = '';
+    $_('.js-pocemon__link', elBasicTemplateClone).dataset.id = pokemon.id;
+    $_('.js-pocemon__title', elBasicTemplateClone).textContent = pokemon.name;
+    $_('.js-pocemon__img', elBasicTemplateClone).src = pokemon.img;
+    $_('.js-pocemon__img', elBasicTemplateClone).alt = pokemon.name;
+    $_('.js-pocemon__img', elBasicTemplateClone).dataset.id = pokemon.id;
+    $_('.js-pokemon__height', elBasicTemplateClone).textContent = pokemon.height;
+    $_('.js-pokemon__genus', elBasicTemplateClone).textContent = pokemon.name;
+    $_('.js-pokemon__kg', elBasicTemplateClone).textContent = pokemon.weight;
+    $_('.js-pokemon__type', elBasicTemplateClone).textContent = pokemon.type.join(' | ');
+
+    elForBasicTemplateFragment.appendChild(elBasicTemplateClone);
+  });
+
+  elResultsList.appendChild(elForBasicTemplateFragment);
+};
+
 var elResultsList = $_('.js-pokemon-list');
 var elBasicTemplate = $_('.js-pocemon__template').content;
-var elForBasicTemplateFragment = document.createDocumentFragment();
-
 
 var elForm = $_('.js-site-header__form');
 var elFormInput = $_('.js-site-header__input');
+
+var pokemonTypeList = $_('.js-pokemons-filter__list');
+
+
+displayResults();
+
+
+elForm.addEventListener('submit', function (evt){
+  evt.preventDefault();
+})
 
 
 elFormInput.addEventListener('input', function(){
@@ -37,46 +71,9 @@ elFormInput.addEventListener('input', function(){
     return pokemon.name.match(inputRegExp);
   });
 
-  elResultsList.innerHTML = '';
-  searchResultFragment = document.createDocumentFragment();
-  searchResult.forEach(function (pokemon) {
-    var elBasicTemplateClone = elBasicTemplate.cloneNode(true);
-
-    $_('.js-pocemon__title', elBasicTemplateClone).textContent = pokemon.name;
-    $_('.js-pocemon__img', elBasicTemplateClone).src = pokemon.img;
-    $_('.js-pocemon__img', elBasicTemplateClone).alt = pokemon.name;
-    $_('.js-pokemon__height', elBasicTemplateClone).textContent = pokemon.height;
-    $_('.js-pokemon__genus', elBasicTemplateClone).textContent = pokemon.name;
-    $_('.js-pokemon__kg', elBasicTemplateClone).textContent = pokemon.weight;
-    $_('.js-pokemon__type', elBasicTemplateClone).textContent = pokemon.type.join(' | ');
-
-    searchResultFragment.appendChild(elBasicTemplateClone);
-  });
-
-  elResultsList.appendChild(searchResultFragment);
+  displayResults(searchResult);
 });
 
-
-
-
-pokemons.forEach(function (pokemon) {
-  var elBasicTemplateClone = elBasicTemplate.cloneNode(true);
-
-  // elResultsList.innerHTML = '';
-  $_('.js-pocemon__link', elBasicTemplateClone).dataset.id = pokemon.id;
-  $_('.js-pocemon__title', elBasicTemplateClone).textContent = pokemon.name;
-  $_('.js-pocemon__img', elBasicTemplateClone).src = pokemon.img;
-  $_('.js-pocemon__img', elBasicTemplateClone).alt = pokemon.name;
-  $_('.js-pocemon__img', elBasicTemplateClone).dataset.id = pokemon.id;
-  $_('.js-pokemon__height', elBasicTemplateClone).textContent = pokemon.height;
-  $_('.js-pokemon__genus', elBasicTemplateClone).textContent = pokemon.name;
-  $_('.js-pokemon__kg', elBasicTemplateClone).textContent = pokemon.weight;
-  $_('.js-pokemon__type', elBasicTemplateClone).textContent = pokemon.type.join(' | ');
-
-  elForBasicTemplateFragment.appendChild(elBasicTemplateClone);
-});
-
-elResultsList.appendChild(elForBasicTemplateFragment);
 
 var pokemonsTypeArray = [];
 
@@ -88,7 +85,7 @@ pokemons.forEach(function (pokemon){
   });
 });
 
-var pokemonTypeList = $_('.js-pokemons-filter__list');
+
 var typeFragment = document.createDocumentFragment();
 
 pokemonsTypeArray.forEach(function (pokemonType){
@@ -110,29 +107,12 @@ pokemonTypeList.addEventListener('click', function (evt) {
     // console.log(evt.target.dataset.id);
     var pokemonTypeItem = pokemons.filter(function (pokemon) {
       var matchCategory = evt.target.dataset.id === "all" || pokemon.type.includes(evt.target.dataset.id);
-      return  matchCategory;
+      return matchCategory;
     })
     // console.log(pokemonTypeItem);
 
-    elResultsList.innerHTML = '';
-    elForBasicTemplateFragmentSecond = document.createDocumentFragment();
-    pokemonTypeItem.forEach(function (pokemon){
-      var elBasicTemplateClone = elBasicTemplate.cloneNode(true);
-
-      $_('.js-pocemon__title', elBasicTemplateClone).textContent = pokemon.name;
-      $_('.js-pocemon__img', elBasicTemplateClone).src = pokemon.img;
-      $_('.js-pocemon__img', elBasicTemplateClone).alt = pokemon.name;
-      $_('.js-pokemon__height', elBasicTemplateClone).textContent = pokemon.height;
-      $_('.js-pokemon__genus', elBasicTemplateClone).textContent = pokemon.name;
-      $_('.js-pokemon__kg', elBasicTemplateClone).textContent = pokemon.weight;
-      $_('.js-pokemon__type', elBasicTemplateClone).textContent = pokemon.type.join(' | ');
-
-      elForBasicTemplateFragmentSecond.appendChild(elBasicTemplateClone);
-    });
-
-    elResultsList.appendChild(elForBasicTemplateFragmentSecond);
+    displayResults(pokemonTypeItem);
   }
-
 });
 
 
@@ -142,13 +122,14 @@ elResultsList.addEventListener('click', function(evt){
   if (evt.target.matches('.js-pocemon__link') || evt.target.matches('img')){
     // console.log(evt.target.dataset.id);
     var pokemonIdItem = pokemons.find(function (pokemon) {
-      return pokemon.id === Number(evt.target.dataset.id);
+      return Number(evt.target.dataset.id) === pokemon.id;
     })
     // console.log(pokemonIdItem);
 
     $_('.js-modal__pokemon-img').src = pokemonIdItem.img;
+    $_('.js-modal__pokemon-img').alt = pokemonIdItem.name;
     $_('.js-modal__pokemon-name').textContent = pokemonIdItem.name;
-    $_('.js-modal__pokemon-type').textContent = pokemonIdItem.type.join(' | ');
+    $_('.js-modal__pokemon-type').textContent = pokemonIdItem.type.join(' |  ');
     $_('.js-modal__pokemon-hight').textContent = pokemonIdItem.height;
     $_('.js-modal__pokemon-weight').textContent = pokemonIdItem.weight;
     $_('.js-modal__pokemon-egg').textContent = pokemonIdItem.egg;
